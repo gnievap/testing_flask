85d260608c562db4064b8a14c064d77341b48003
Gaby Nieva <gaby.nieva@outlook.com>
2024-06-30 23:05:24 -0600

    Error handlers

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from flask import Flask, make_response, redirect, render_template, request
 
 app = Flask(__name__)
 
-players = ['Patrick Mahomes', 'Travis Kelce', 'Isah Pacheco','Derrick Nadhi', 'George Karlaftis']
+players = ['Patrick Mahomes', 'Travis Kelce', 'Isah Pacheco','Derrick Nnadi', 'George Karlaftis']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
 
 @app.route('/')
 def index():
