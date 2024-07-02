6cc433fc7a43100519a4e4e3552c8b8d68c1605e
Gaby Nieva <gaby.nieva@outlook.com>
2024-07-01 22:45:06 -0600

    Forms WTF

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import (Flask, make_response, redirect, render_template, request,
                    session)
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -9,6 +12,11 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 
 players = ['Patrick Mahomes', 'Travis Kelce', 'Isah Pacheco','Derrick Nnadi', 'George Karlaftis']
+
+class LoginForm (FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -32,9 +40,11 @@ def index():
 def hello():
     # user_ip = request.cookies.get('user_ip')
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip':user_ip,
         'players':players,
+        'login_form': login_form
     }
 
     # user_ip = request.remote_addr
